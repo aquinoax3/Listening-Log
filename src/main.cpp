959416fa9e31b657900420e8332fa6685e090b3c
aquinoax3 <aquino.axel@gmail.com>
2025-06-20 07:51:57 +0100

    Added todo list for the next tasks

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,9 @@ int main() {
  CROW_ROUTE(app,"/")([](){
 		 return "Hello World";
 	});
-
+ //TODO: Create POST endpoint for log
+ //TODO: Create GET endpoint for log
+  
  app.port(18080).multithreaded().run();
 
 }
