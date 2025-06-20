b36c2d65ebedc023780d34d592348a9bd67287b2
aquinoax3 <aquino.axel@gmail.com>
2025-06-20 07:42:06 +0100

    Created server and boilerplate endpoint

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include "../include/crow_all.h"
+
+
+int main() {
+
+ crow::SimpleApp app;
+
+ CROW_ROUTE(app,"/")([](){
+		 return "Hello World";
+	});
+
+ app.port(18080).multithreaded().run();
+
+}
+
